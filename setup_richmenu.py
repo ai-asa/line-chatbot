cc76331bb5ed7267e3932afcc093eca6a8b353d1
asus <asatechblog@gmail.com>
2025-02-14 12:16:37 +0900

    Update rich_menu's id

--- setup_richmenu.py
+++ setup_richmenu.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     # rt.delete_all_richmenus_and_aliases()
     # imagepath_list = ["./data/richmenu/images/機能選択画面.png","./data/richmenu/images/契約画面.png"]
     # rt.gen_richmenu(imagepath_list)
-    rt.change_richmenu_default(1,"richmenu-e5d1166474227499f1a9a5244f7eb3b9")
+    rt.change_richmenu_default(1,"richmenu-2638d235c437db8d60690fdb75133ae2")
     rt.get_default_richmenu()
-    rt.get_richmenu_alias("richmenu-alias-a","richmenu-e5d1166474227499f1a9a5244f7eb3b9")
-    rt.get_richmenu_alias("richmenu-alias-b","richmenu-a8e4d815bb0ec951068d253e09370814")
+    rt.get_richmenu_alias("richmenu-alias-a","richmenu-2638d235c437db8d60690fdb75133ae2")
+    rt.get_richmenu_alias("richmenu-alias-b","richmenu-83adfd9fcfa159fd772cbebdb09f7cfe")
 
 
 # %%
