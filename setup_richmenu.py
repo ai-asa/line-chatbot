c0edda0b2f5b1562dbe03ee6f223732c9e8dfc64
asus <asatechblog@gmail.com>
2025-03-14 15:12:24 +0900

    いくつかの修正
    ・リッチメニュー画像の名前にスペースが含まれていた点を修正
    ・requirements.txtの一部モジュールのバージョンを修正

--- setup_richmenu.py
+++ setup_richmenu.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     # rt.change_richmenu_default(1)
     # rt.cancel_richmenu_default()
     # rt.delete_all_richmenus_and_aliases()
-    # imagepath_list = ["./data/richmenu/images/機能選択画面v3.png","./data/richmenu/images/契約画面v3.png"]
+    # imagepath_list = ["./data/richmenu/images/リッチメニュー_準備中v1.png","./data/richmenu/images/リッチメニュー_準備中v2.png"]
     # rt.gen_richmenu(imagepath_list)
-    rt.change_richmenu_default(1,"richmenu-ab1b74660e35e87b457cde77df8549f2")
+    rt.change_richmenu_default(1,"richmenu-6bf32ec02a0d404abac4c7f62010ae26")
     rt.get_default_richmenu()
-    rt.get_richmenu_alias("richmenu-alias-a","richmenu-ab1b74660e35e87b457cde77df8549f2")
-    rt.get_richmenu_alias("richmenu-alias-b","richmenu-db51ad0feaecb6e1334ecf2e4e3d0f52")
+    rt.get_richmenu_alias("richmenu-alias-a","richmenu-6bf32ec02a0d404abac4c7f62010ae26")
+    rt.get_richmenu_alias("richmenu-alias-b","richmenu-ddeccbf6ed74440023f19998985d6d3a")
 
 
 # %%
