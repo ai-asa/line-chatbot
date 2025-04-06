0e3f91f3d00d02af113144c45b9af796884dbbaf
asus <asatechblog@gmail.com>
2025-04-06 23:28:35 +0900

    リッチメニューの更新作業

--- setup_richmenu.py
+++ setup_richmenu.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     # rt.delete_all_richmenus_and_aliases()
     # imagepath_list = ["./data/richmenu/images/リッチメニュー_準備中v1.png","./data/richmenu/images/リッチメニュー_準備中v2.png"]
     # rt.gen_richmenu(imagepath_list)
-    rt.change_richmenu_default(1,"richmenu-6bf32ec02a0d404abac4c7f62010ae26")
+    rt.change_richmenu_default(1,"richmenu-9e5c3697a9eaa11e73728a3f2c592829")
     rt.get_default_richmenu()
-    rt.get_richmenu_alias("richmenu-alias-a","richmenu-6bf32ec02a0d404abac4c7f62010ae26")
-    rt.get_richmenu_alias("richmenu-alias-b","richmenu-ddeccbf6ed74440023f19998985d6d3a")
+    rt.get_richmenu_alias("richmenu-alias-a","richmenu-9e5c3697a9eaa11e73728a3f2c592829")
+    rt.get_richmenu_alias("richmenu-alias-b","richmenu-75b3e40b87858e1fbeb9dcba61c82d32")
 
 
 # %%
